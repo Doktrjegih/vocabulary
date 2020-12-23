0dfdc15d7f2787a9f5ced1188a4b1b175615a16c
Doktrjegih <doktrjegih@gmail.com>
2020-12-23 23:33:47 +0500

    it works?

--- test2.py
+++ test2.py
@@ -16,7 +16,11 @@ with open("test.csv", encoding='utf-8') as r_file:
             print('great!')
             with open("test.csv", "r") as file:
                 lines = file.readlines()
-                print(lines)
+            for line in lines:
+                if oldest_records[count][2] in line:
+                    lines.remove(line)
+            with open("test.csv", "w") as file:
+                file.writelines(lines)
             with open("test.csv", mode="a", encoding='utf-8') as w_file:
                 file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
                 oldest_records[count][2] = now.isoformat()
