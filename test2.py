c646790b7723e70439e736a2d5cc0ef448b5460c
Doktrjegih <doktrjegih@gmail.com>
2020-12-24 22:49:35 +0500

    test3

--- test2.py
+++ test2.py
@@ -2,17 +2,15 @@ import csv
 import random
 import datetime
 
-print('test server_side2')
-
 with open("test.csv", encoding='utf-8') as r_file:
-    reader = csv.reader(r_file, delimiter=";")
+    reader = csv.reader(r_file, delimiter=",")
     sortedlist = sorted(reader, key=lambda row: row[2], reverse=False)
     oldest_records = sortedlist[0:3]
     for i in range(3):
         count = random.randint(0, len(oldest_records) - 1)
         print(oldest_records[count][0])
         answer = input("answer: ")
-        if answer == oldest_records[count][1]:
+        if answer.lower() == oldest_records[count][1].lower():
             now = datetime.datetime.now()
             print('great!')
             with open("test.csv", "r") as file:
@@ -23,7 +21,7 @@ with open("test.csv", encoding='utf-8') as r_file:
             with open("test.csv", "w") as file:
                 file.writelines(lines)
             with open("test.csv", mode="a", encoding='utf-8') as w_file:
-                file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
+                file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
                 oldest_records[count][2] = now.isoformat()
                 file_writer.writerow(oldest_records[count])
         else:
